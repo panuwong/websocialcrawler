81e443a187d96648629f0b64f0e967d3563d8654
Dewo D. Benedix <c.panuwong@gmail.com>
2019-12-02 14:12:34 +0700

    init2

--- configDB/db.js
+++ configDB/db.js
@@ -1,6 +1,6 @@
 var mongojs = require('mongojs');
 
-var databaseUrl = 'mongodb://ivb:ivb2018!ja@159.65.141.81:54302/mandala_db?authSource=admin';
+var databaseUrl = '';
 var collections = ['c2-index','voice_facebook','voice_instagram','index_repo_campaign','voice_pantip','youtube-voice-test','zprimarykey_voice_instagram'];
 
 
